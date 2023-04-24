eb53451db7514b179912a43781186f1e88d72504
Danny Zheng <dannylobby123@gmail.com>
2023-04-23 21:56:23 -0400

    external library commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function injectInfo(list) {
+    console.log("fired injectHTML");
+    const target = document.querySelector("#info");
+    target.innerHTML = "";
+    list.forEach((item, index) => {
+      const str = `<li>${item.name}</li>`;
+      target.innerHTML += str;
+    });
+  }
+
+async function getData() {
+    const pokemon = document.getElementById("pokemonName").value;
+    console.log('pokemon is:', pokemon);
+    const url = new URL("https://pokeapi.co/api/v2/pokemon/" + pokemon);
+    console.log("API Url:", url.toString());
+
+    const response = await fetch(url);
+
+    // console.log(response);
+
+    const data = await response.json();
+
+    console.log(data);
+
+}
